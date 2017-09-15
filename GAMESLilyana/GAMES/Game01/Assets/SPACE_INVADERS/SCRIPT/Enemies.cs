3afafcb17dd75c28b5313b563387999cd5063943
Lilian48 <lilyan48@hotmail.com>
2017-09-14 21:56:19 -0500

    14/09/17
    scriptEnemigosSpawn

--- GAMESLilyana/GAMES/Game01/Assets/SPACE_INVADERS/SCRIPT/Enemies.cs
+++ GAMESLilyana/GAMES/Game01/Assets/SPACE_INVADERS/SCRIPT/Enemies.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class Enemies : MonoBehaviour {
 
+	[SerializeField] GameObject Enemy;
+
+
 	// Use this for initialization
 	void Start () {
 		
+		
+				for(int i=-5; i <5 ; i ++){
+					for (int s = -1; s <3 ; s++) {
+
+						Instantiate (Enemy, new Vector3 (i, s, 0), Quaternion.identity);
+			}
+		}
 	}
 	
 	// Update is called once per frame
